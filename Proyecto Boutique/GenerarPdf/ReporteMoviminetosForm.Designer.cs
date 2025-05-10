5544d51d62c40a6b0d770cb12b5e5130b96c629e
Martin Vazquez <vazquezlmartin33@gmail.com>
2025-05-09 21:56:54 -0700

    pedf

---
+++ Proyecto Boutique/GenerarPdf/ReporteMoviminetosForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Proyecto_Boutique.Forms.GenerarPDF
+{
+    partial class ReporteMoviminetosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpFechaDesde = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaHasta = new System.Windows.Forms.DateTimePicker();
+            this.cmbTipoMovimiento = new System.Windows.Forms.ComboBox();
+            this.Generar_Click = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpFechaDesde
+            // 
+            this.dtpFechaDesde.Location = new System.Drawing.Point(21, 94);
+            this.dtpFechaDesde.Name = "dtpFechaDesde";
+            this.dtpFechaDesde.Size = new System.Drawing.Size(318, 26);
+            this.dtpFechaDesde.TabIndex = 0;
+            // 
+            // dtpFechaHasta
+            // 
+            this.dtpFechaHasta.Location = new System.Drawing.Point(400, 94);
+            this.dtpFechaHasta.Name = "dtpFechaHasta";
+            this.dtpFechaHasta.Size = new System.Drawing.Size(317, 26);
+            this.dtpFechaHasta.TabIndex = 1;
+            // 
+            // cmbTipoMovimiento
+            // 
+            this.cmbTipoMovimiento.FormattingEnabled = true;
+            this.cmbTipoMovimiento.Location = new System.Drawing.Point(12, 156);
+            this.cmbTipoMovimiento.Name = "cmbTipoMovimiento";
+            this.cmbTipoMovimiento.Size = new System.Drawing.Size(192, 28);
+            this.cmbTipoMovimiento.TabIndex = 2;
+            // 
+            // Generar_Click
+            // 
+            this.Generar_Click.Location = new System.Drawing.Point(294, 233);
+            this.Generar_Click.Name = "Generar_Click";
+            this.Generar_Click.Size = new System.Drawing.Size(98, 34);
+            this.Generar_Click.TabIndex = 3;
+            this.Generar_Click.Text = "Generar";
+            this.Generar_Click.UseVisualStyleBackColor = true;
+            this.Generar_Click.Click += new System.EventHandler(this.Generar_Click_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tipo de Movimiento:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Reportes de Movimiento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Desde:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(489, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Hasta:";
+            // 
+            // ReporteMoviminetosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 307);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Generar_Click);
+            this.Controls.Add(this.cmbTipoMovimiento);
+            this.Controls.Add(this.dtpFechaHasta);
+            this.Controls.Add(this.dtpFechaDesde);
+            this.Name = "ReporteMoviminetosForm";
+            this.Text = "ReporteMoviminetosForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpFechaDesde;
+        private System.Windows.Forms.DateTimePicker dtpFechaHasta;
+        private System.Windows.Forms.ComboBox cmbTipoMovimiento;
+        private System.Windows.Forms.Button Generar_Click;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Proyecto Boutique/PDFGenerador.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+
+namespace Proyecto_Boutique
+{
+    internal class PDFGenerador
+    {
+        public static void GeneratePdfFromHtml(string html, string filePath)
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Create))
+            {
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+
+                using (StringReader sr = new StringReader(html))
+                {
+                    HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+                    htmlparser.Parse(sr);
+                }
+
+                pdfDoc.Close();
+                stream.Close();
+            }
+        }
+        public static void ShowSaveDialogAndGenerate(string html, string defaultFileName)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "PDF files (*.pdf)|*.pdf",
+                FileName = defaultFileName
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                GeneratePdfFromHtml(html, saveFileDialog.FileName);
+                MessageBox.Show("Reporte generado exitosamente!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
+
